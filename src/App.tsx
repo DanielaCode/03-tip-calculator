d3113fa3aeacf5177e32675e4ab7687204369b87
Daniela <danielagp96@gmail.com>
2024-04-16 08:23:16 -0600

    Adding main containers

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ function App() {
       <header className="bg-sky-400 py-5">
         <h1 className="text-center text-3xl font-black">Calculadora de precios</h1>
       </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>menu</h2>
+        </div>
+        <div>
+          <h2>consumos</h2>
+          
+        </div>
+      </main>
     </>
   );
 }
