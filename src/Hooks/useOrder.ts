717527a86d0a3358c5b0672bb90a2066f4592855
Daniela <danielagp96@gmail.com>
2024-04-16 10:00:22 -0600

    if same MenuItem is pressed the state will duplicate so a conditional to controlled it is added then instead it will update the quantity

--- src/Hooks/useOrder.ts
+++ src/Hooks/useOrder.ts
@@ -5,9 +5,17 @@ function useOrder() {
   const [order, setOrder] = useState<OrderItem[]>([]);
 
   function addItem(item:MenuItem){
-    //casting an MenuItem to OrderItem
-    const newItem:OrderItem = {...item,quantity:1}
-    setOrder([...order, newItem]);
+    //check if the item exist in the state
+    const itemExist = order.find(e=>e.id===item.id);
+    if (itemExist) {
+        const updatedOrder = order.map(e=>e.id==item.id?{...e,quantity:e.quantity+1}:e);
+        setOrder(updatedOrder);
+    } else {
+        //casting an MenuItem to OrderItem
+        const newItem:OrderItem = {...item,quantity:1}
+        setOrder([...order, newItem]);
+    }
+   
   }
 
   return {
